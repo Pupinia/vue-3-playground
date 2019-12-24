2fb32523b0322a4a448896fd553064bfab14893f
ivan <ivangorovoyyy@gmail.com>
2019-12-24 17:34:05 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueCompositionApi from "@vue/composition-api";
 
-Vue.config.productionTip = false
+Vue.use(VueCompositionApi);
+
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
